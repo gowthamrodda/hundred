c94c238eb71f8026db317989162bcb3c8b2d3be3
Gowtham Kumar Rodda <gowtham.kumar@legatohealth.com>
2022-03-14 11:56:48 +0530

    issue#1 initialize spring boot

---
+++ src/main/java/com/codechallenge/hundred/HundredApplication.java
@@ -0,0 +1,13 @@
+package com.codechallenge.hundred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HundredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HundredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallenge/hundred/HundredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.hundred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HundredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
